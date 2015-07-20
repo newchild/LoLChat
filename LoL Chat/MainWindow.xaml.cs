255f55c600617e5c95a1f0062f29220369398a3c
RoachxD <roachxxxx@gmail.com>
2015-07-20 05:07:00 +0300

    New Shit :D  - More to come tomorrow, I'm too tired :)  - Do your magic until then (I mean the Chat functions and shits, I will fix the Throwing Errors tomorrow) :P

---
+++ LoL Chat/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using MahApps.Metro.Controls;
+
+namespace LoL_Chat
+{
+    /// <summary>
+    ///     Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
